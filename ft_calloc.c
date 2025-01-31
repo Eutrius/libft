8582bce125fc8bb5c591fe9945ca32e1e42cfd3f
John Vincent Yriarte <jyriarte@e3r8p1.42roma.it>
2025-01-31 14:37:20 +0100

    original libft

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 22:09:54 by jyriarte          #+#    #+#             */
+/*   Updated: 2024/08/19 22:10:06 by jyriarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*buff;
+
+	if (nmemb != 0 && size > SIZE_MAX / nmemb)
+		return (NULL);
+	buff = malloc(nmemb * size);
+	if (!buff)
+		return (NULL);
+	ft_bzero(buff, nmemb * size);
+	return (buff);
+}
